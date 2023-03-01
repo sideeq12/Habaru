2d033549c9be3e9fb483906b98b18558e10eb25c
sideeq12 <abdwaheed2018@gmail.com>
2023-03-01 13:27:10 +0000

    testing StepZen

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import { categories } from '../constant'
+import fetchNews from '../lib/fetchNews';
 
 async function HomePage() {
-    const news : NewsResponse = await fetchNews(categories.join(","))
+    // const news : NewsResponse = await fetchNews(categories.join(","))
   return (
     <div>HomePage</div>
   )
